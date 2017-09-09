58fbc20f1d78bb0d531ee436541058e127a0af3e
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-09-09 16:40:59 -0500

    Successfully collect days of week in table with id

--- tracker.rb
+++ tracker.rb
@@ -3,16 +3,26 @@
 require 'sqlite3'
 
 
-tracker = SQLite3::Database.new("days_of_the_week.db")
-
-
+db = SQLite3::Database.new("days.db")
 
 # create a days of the week table (foreign key ref in calorie tracker table)
-create_days_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS days_of_the_week(
+create_days_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS days(
     id INTEGER PRIMARY KEY,
-    day VARCHAR (255)
+    weekday VARCHAR (255)
   )
 SQL
+
+db.execute(create_days_table)
+
+def add_days(db, weekday)
+  db.execute("INSERT INTO days (weekday) VALUES (?)", [weekday])
+end
+
+days_array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+days_array.each do |weekday|
+  add_days(db, weekday)
+end
+
 
 
