451d0345120acd646ce9aa08a1129a3c0dbcce06
Nikita Ahuja <nikitaahuja@Nikitas-MacBook-Pro.local>
2017-09-09 16:42:42 -0500

    rename days database for clarity

--- tracker.rb
+++ tracker.rb
@@ -3,7 +3,7 @@
 require 'sqlite3'
 
 
-db = SQLite3::Database.new("days.db")
+db_days = SQLite3::Database.new("days.db")
 
 # create a days of the week table (foreign key ref in calorie tracker table)
 create_days_table = <<-SQL
@@ -13,15 +13,15 @@ create_days_table = <<-SQL
   )
 SQL
 
-db.execute(create_days_table)
+db_days.execute(create_days_table)
 
-def add_days(db, weekday)
-  db.execute("INSERT INTO days (weekday) VALUES (?)", [weekday])
+def add_days(db_days, weekday)
+  db_days.execute("INSERT INTO days (weekday) VALUES (?)", [weekday])
 end
 
 days_array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 days_array.each do |weekday|
-  add_days(db, weekday)
+  add_days(db_days, weekday)
 end
 
 
